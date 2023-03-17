dd71dae565278e483c0604bc70401f9a77619274
Wojciech Peisert <wojtekp@sauron.pl>
2023-03-17 14:58:32 +0100

    rename files: agent into base_agent and environment into base_environment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from bridgezero.agent import BaseAgent
-from bridgezero.environment import BaseEnvironment
+from bridgezero.base_agent import BaseAgent
+from bridgezero.base_environment import BaseEnvironment
 from bridgezero.rl_glue import RLGlue
 from bridgezero.state_features import StateFeatures
 
