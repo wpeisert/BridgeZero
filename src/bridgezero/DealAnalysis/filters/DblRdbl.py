0e75efe3e68f164b0f5a8647c46e2297143fb3bc
Wojciech Peisert <wojtekp@sauron.pl>
2023-03-22 15:52:05 +0100

    DblRdbl.filter()

--- src/bridgezero/DealAnalysis/filters/DblRdbl.py
+++ src/bridgezero/DealAnalysis/filters/DblRdbl.py
@@ -1,5 +1,65 @@
+import copy
+
+from bridgezero import constants
+
+
 class DblRdbl:
     @staticmethod
-    def filter(contracts):
-        # TODO
-        pass
+    def filter(contractsIn):
+        contractsFiltered = {}
+
+        for contractIn in contractsIn:
+            contract = contractIn['contract']
+
+            coeff = 1 if contract.declarer in 'NS' else -1
+            contracts = []
+            evs = []
+            data = []
+            for type in constants.CONTRACT_TYPES:
+                contractTmp = copy.deepcopy(contract)
+                contractTmp.type = type
+                hash = contractTmp.getHash()
+                exists = hash in contracts[hash]
+                contracts.append(contractTmp)
+                data.append(coeff * contracts[hash]['ev'] if exists else None)
+                evs.append(contracts[hash]['ev'] if exists else None)
+
+            indices = DblRdbl.getRemainingIndices(*data)
+
+            for index in indices:
+                contractsFiltered[contracts[index].getHash()] = {
+                    'contract': contracts[index],
+                    'ev': evs[index],
+                }
+
+        return contractsFiltered
+
+
+    @staticmethod
+    def getRemainingIndices(val0, val1, val2):
+        if (
+            # if no more than 1, returns same
+            ((1 if val0 is not None else 0) + (1 if val1 is not None else 0) + (1 if val2 is not None else 0) <= 1) or
+            # only first and last ('' and 'rdbl')
+            ((val0 is not None) and (val1 is None) and (val2 is not None))
+        ):
+            res = []
+            for index,val in enumerate([val0, val1, val2]):
+                if val is not None:
+                    res.append(index)
+            return res
+
+        # val1 is not null !!
+
+        if val2 is None:
+            return [1] if val0 > val1 else [0]
+
+        if val0 is None:
+            return 2 if val2 > val1 else [1]
+
+        # all not null
+
+        if (val0 < val1 or val2 > val0):
+            return [0]
+        else:
+            return [2] if val2 > val1 else [1]
--- src/bridgezero/DealAnalysis/filters/SameResultForBothDeclarersInSide.py
+++ src/bridgezero/DealAnalysis/filters/SameResultForBothDeclarersInSide.py
@@ -5,14 +5,14 @@ from bridgezero.Bidding.Tools import Tools
 
 class SameResultForBothDeclarersInSide:
     @staticmethod
-    def filter(contractsEvaluated):
+    def filter(contractsIn):
         contractsFiltered = {}
 
-        for hash1, contractEvaluated in contractsEvaluated.items():
-            contract1 = copy.deepcopy(contractEvaluated['contract'])
-            ev1 = contractEvaluated['ev']
+        for hash1, contractIn in contractsIn.items():
+            contract1 = copy.deepcopy(contract['contract'])
+            ev1 = contractIn['ev']
 
-            contract2 = contractEvaluated['contract']
+            contract2 = contractIn['contract']
             contract2.declarer = Tools.getPartner(contract2.declarer)
             hash2 = contract2.getHash()
 
@@ -23,7 +23,7 @@ class SameResultForBothDeclarersInSide:
                 }
             continue
 
-            ev2 = contractsEvaluated[hash2]['ev']
+            ev2 = contractsIn[hash2]['ev']
 
             if abs(ev1 - ev2) < 0.001:
                 contract1.declarer = Tools.getFirstPlayerInSide(contract1.declarer)
