9ed4cc575d2af2ad170e10700147de7674682c66
Wojciech Peisert <wojtekp@sauron.pl>
2023-03-28 12:59:25 +0200

    StateFeatures: colors, pc, vulnerable - implementation & tests

--- src/bridgezero/state_features.py
+++ src/bridgezero/state_features.py
@@ -1,7 +1,9 @@
 import numpy as np
 from bridgezero import constants
 from bridgezero.base.base_state_features import BaseStateFeatures
+from bridgezero.hand import Hand
 from bridgezero.state import State
+
 
 class StateFeatures(BaseStateFeatures):
     """
@@ -38,24 +40,75 @@ class StateFeatures(BaseStateFeatures):
     Features are sparse. Most of them are binary with exceptions for count of passes, dbls and rdbls bid by a player.
     """
 
-    def __init__(self, colors_tiles_width = 2, pc_tiles_width = 3, last_bids_count = 3):
+    def __init__(self, **kwargs):
         """
         :param colors_tiles_width: Width of intervals for player cards of given color
         :param pc_tiles_width: Width of intervals for player PCs
         :param last_bids_count: Count of last bids in feature vector
         """
-        self.colors_tiles_width = colors_tiles_width
-        self.pc_tiles_width = pc_tiles_width
-        self.last_bids_count = last_bids_count
+        self.colors_tiles_width = kwargs.get("colors_tiles_width", 2)
+        self.pc_tiles_width = kwargs.get("pc_tiles_width", 3)
+        self.last_bids_count = kwargs.get("last_bids_count", 3)
 
-    def get_features(self, state : State):
-        raise NotImplementedError("StateFeatures.get_features not implemented")
-        return np.array([0] * 373) # 373 - default number of features
+        self.state = None
+        self.features = None
 
     def get_features_count(self):
         return \
-            constants.PLAYERS_COUNT * (constants.CARDS_IN_COLOR_COUNT + 1 + self.colors_tiles_width) + \
+            constants.COLORS_COUNT * (constants.CARDS_IN_COLOR_COUNT + 1 + self.colors_tiles_width) + \
             constants.MAX_PC_PLAYER + 1 + self.pc_tiles_width + \
             2 + \
             constants.PLAYERS_COUNT * constants.ALL_POSSIBLE_BIDS_COUNT + \
             constants.ALL_POSSIBLE_BIDS_COUNT * self.last_bids_count
+
+    def get_features(self, state : State):
+        # raise NotImplementedError("StateFeatures.get_features not implemented")
+        # return np.array([0] * 373) # 373 - default number of features
+        self.state = state
+        self.features = np.zeros(self.get_features_count())
+        self._add_colors()
+        self._add_pc()
+        self._add_vulnerable()
+
+        return self.features
+
+    def _add_colors(self):
+        for color in range(constants.COLORS_COUNT):
+            self._add_color(color)
+
+    def _add_color(self, color):
+        """
+        - tilings for each color: (-colors_tiles_width) - 0, (-colors_tiles_width+1) - 1, ..., 0 - colors_tiles_width, 1 - (colors_tiles_width+1), ..., 13 - (colors_tiles_width+13);
+        this gives BINARY features cnt: (14 + colors_tiles_width) features for each color (i. e. x4 colors)
+
+        This means that:
+        1. There is (constants.CARDS_IN_COLOR_COUNT + 1 + self.colors_tiles_width) fields (tiles) for each color
+        2. Each state belongs to (self.colors_tiles_width+1) fields (tiles)
+        """
+        features_per_color = constants.CARDS_IN_COLOR_COUNT + 1 + self.colors_tiles_width
+        interval_start = color * features_per_color + Hand.getColorCardsCount(self.state.hand, color)
+        interval_length = self.colors_tiles_width + 1
+        for inx in range(interval_start, interval_start + interval_length):
+            self.features[inx] = 1
+
+    def _add_pc(self):
+        """
+        - tilings for PCs: (-pc_tiles_width) - 0, (-pc_tiles_width+1) - 1, ..., 0-pc_tiles_width, 1-(pc_tiles_width+1), ..., 37 - (pc_tiles_width+37);
+        this gives BINARY features cnt: 37 + pc_tiles_width
+        """
+        features_per_color = constants.CARDS_IN_COLOR_COUNT + 1 + self.colors_tiles_width # skip colors (TODO - refactor)
+        interval_start = constants.COLORS_COUNT * features_per_color + Hand.getPc(self.state.hand)
+        interval_length = self.pc_tiles_width + 1
+        for inx in range(interval_start, interval_start + interval_length):
+            self.features[inx] = 1
+
+    def _add_vulnerable(self):
+        """
+        - weVuln, theyVuln - 1/0;
+        this gives BINARY features cnt: 2
+        """
+        features_per_color = constants.CARDS_IN_COLOR_COUNT + 1 + self.colors_tiles_width
+        features_for_pc = constants.MAX_PC_PLAYER + self.pc_tiles_width + 1
+        vulnerable_start = constants.COLORS_COUNT * features_per_color + features_for_pc # skip colors, PC (TODO - refactor)
+        self.features[vulnerable_start] = 1 if self.state.we_vulnerable else 0
+        self.features[vulnerable_start + 1] = 1 if self.state.they_vulnerable else 0
--- tests/test_bridgezero/test_state_features.py
+++ tests/test_bridgezero/test_state_features.py
@@ -1,5 +1,45 @@
+import numpy as np
+
+from bridgezero.state import State
 from bridgezero.state_features import StateFeatures
 
-def test1():
+def test_number_of_features_default():
     stateFeatures = StateFeatures()
     assert stateFeatures.get_features_count() == 373
+    assert stateFeatures.get_features_count() == 16*4 + 41 + 2 + 4*38 + 3*38
+
+def test_number_of_features():
+    stateFeatures = StateFeatures(colors_tiles_width=1, pc_tiles_width=1, last_bids_count=1)
+    assert stateFeatures.get_features_count() == 15*4 + 39 + 2 + 4*38 + 1*38
+
+def test_empty():
+    stateFeatures = StateFeatures()
+    state = State(hand=[], bidding=[], we_vulnerable=False, they_vulnerable=False)
+    features = stateFeatures.get_features(state)
+    assert np.sum(features) == 4 * 3 + 4
+    assert features.tolist() == ([1,1,1] + [0]*13) * 4 + [1,1,1,1] + [0]*37 + [0,0] + [0]*4*38 + [0]*3*38
+
+def test_example_hand_colors_pc():
+    stateFeatures = StateFeatures()
+    state = State(hand=[0,1,2,13,14,15], bidding=[], we_vulnerable=True, they_vulnerable=False)
+    #state = State(hand=[5, 6, 7, 17, 18, 19], bidding=[], we_vulnerable=True, they_vulnerable=False)
+    features = stateFeatures.get_features(state)
+    assert np.sum(features) == 4 * 3 + 4 + 1
+
+    actual = features.tolist()
+    expected = ([0,0,0,1,1,1] + [0]*10) * 2 + ([1,1,1] + [0]*13) * 2 + [0]*18 + [1,1,1,1] + [0]*19 + [1,0] + [0]*4*38 + [0]*3*38
+
+    assert actual == expected
+
+def test_vulnerable():
+    stateFeatures = StateFeatures()
+    for we_vulnerable in [True, False]:
+        for they_vulnerable in [True, False]:
+            state = State(hand=[5, 6, 7, 17, 18, 19], bidding=[], we_vulnerable=we_vulnerable, they_vulnerable=they_vulnerable)
+            features = stateFeatures.get_features(state)
+
+            actual = features.tolist()
+            expected = ([0,0,0,1,1,1] + [0]*10) * 2 + ([1,1,1] + [0]*13) * 2 + [1,1,1,1] + [0]*37 +\
+                       [1 if we_vulnerable else 0,1 if they_vulnerable else 0] + [0]*4*38 + [0]*3*38
+
+            assert actual == expected
